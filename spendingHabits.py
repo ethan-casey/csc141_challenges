8f89640280c2a41bd66c0a1bad46eff4de968512
ethan-casey <ethancasey2@gmail.com>
2024-09-05 10:12:29 -0400

    extra credit

---
+++ spendingHabits.py
@@ -0,0 +1,16 @@
+# Ethan Casey
+# Date: 9/5/24
+# This is the extra credit thing 
+
+
+PA_TAX = 0.06
+PRICE_OF_MEAL = 12.49
+
+
+
+print("| Lunch Spending Habits |")
+print("------------------------")
+print(f"tax per meal: {PA_TAX}")
+print(f"total without tax: {PRICE_OF_MEAL*5}")
+print(f"total with tax: {(PRICE_OF_MEAL+PA_TAX)*5}")
+print("------------------------")
---
+++ variables.py
@@ -0,0 +1,10 @@
+# Ethan Casey
+# Date: 9/5/24
+# This is extra credit
+
+
+print("To generate your introduction, I will need some information from you...\n")
+print("Your name? Ethan")
+print("Your age? 20")
+print("What do you study? Game and sim")
+print("\nhi, my name is ethan, i am 20 years old, and i study game and sim at albright college".title())
